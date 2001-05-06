73e937b29ee12a3e1936f3d64c7e92b3e8edba8d
Peter Bortas <zino@lysator.liu.se>
2001-05-06 02:23:32 +0000

    Well...

--- examples/ex_serial.c
+++ examples/ex_serial.c
@@ -1,4 +1,4 @@
-#include "../ronin.h"
+#include "serial.h"
 
 int main(int argc, char **argv)
 {
@@ -7,32 +7,7 @@ int main(int argc, char **argv)
 
   serial_init(57600);
   serial_puts("Hello World!\n");
-  serial_flush();
-
-#if 0
-  serial_puts("What is your name? ");
-  serial_flush();
-  
-  for(i=0; i<19; i++) {
-    serial_puts(".\n");
-    serial_flush();
-    name[i] = serial_getc_blocking();
-    if(name[i] == '\n' || name[i] == '\r') {
-      name[i] = 0;
-      break;
-    }
-  }
-  name[19] = 0;
-    
-  serial_puts("\nHi there ");
-  serial_puts(name);
-  serial_putc('\n');
-  serial_flush();
-  
-  serial_puts("Enter q to quit.\n");
-  serial_flush();
-  while(serial_getc() != 'q') {/* Busy loop */;}
-#endif
+  serial_flush(); //This is vital unless you aim for a hung application.
 
   return 0;
 }
--- examples/ex_video.c
+++ examples/ex_video.c
@@ -1,8 +1,9 @@
-#include "../ronin.h"
+#include "video.h"
 
 int main(int argc, char *argv)
 {
   dc_setup_ta();
 
+  usleep(1000000);
   return 0;
 }
