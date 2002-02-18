ca3bc7526ac5145ee4c7e7f5011a9df2dbee1100
Peter Bortas <zino@lysator.liu.se>
2002-02-18 19:26:25 +0000

    Prototypes for various malloc-stuff.

--- notlibc.h
+++ notlibc.h
@@ -10,11 +10,31 @@
 #include "common.h"
 START_EXTERN_C
 void exit(int rcode) /*__THROW*/ __attribute__ ((__noreturn__));
-void *malloc(size_t size);
 void free(void *ptr);
 int __offtime(const long int *t, long int offset, struct tm *tp);
 int brk( void *ebdds );
 void *sbrk( size_t incr );
+/*  Standard (ANSI/SVID/...)  functions: */
+void *malloc(size_t size);
+void *calloc(size_t nelem, size_t elsize);
+void free(void *ptr);
+void *realloc(void *ptr, size_t size);
+void *memalign(size_t alignment, size_t size);
+void *valloc(size_t size);
+void mallinfo(void);
+int mallopt(int cmd, int value);
+
+/* 
+  Additional functions:
+    independent_calloc(size_t n_elements, size_t size, Void_t* chunks[]);
+    independent_comalloc(size_t n_elements, size_t sizes[], Void_t* chunks[]);
+    pvalloc(size_t n);
+    cfree(Void_t* p);
+    malloc_trim(size_t pad);
+    malloc_usable_size(Void_t* p);
+    malloc_stats();
+*/
+
 END_EXTERN_C
 
 #endif //_NOTLIBC_H
