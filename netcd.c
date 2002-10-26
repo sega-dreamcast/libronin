5a4ac9c2270e77fad62925453472aed6ca5dbc4e
Per Hedbor <per@hedbor.org>
2002-10-26 19:26:09 +0000

    Debug.

--- netcd.c
+++ netcd.c
@@ -68,9 +68,9 @@ static int docmd(int command, const void *data, int sz)
 EXTERN_C int open(const char *path, int mode, ...)
 {
   int res;
-  /*  printf("open(%s,%d)\n", path, mode); */
+  printf("open(%s,%d)\n", path, mode); 
   res = docmd(1, path, strlen(path));
-  /* printf("res = %d\n", res); */
+  printf("res = %d\n", res); 
   if(res>=0 && res<=MAXFD)
     readpos[res]=0;
   else
@@ -81,9 +81,9 @@ EXTERN_C int open(const char *path, int mode, ...)
 EXTERN_C int close(int fd)
 {
   int res;
-  /* printf("close(%d)\n", fd); */
+  printf("close(%d)\n", fd);
   res = docmd(3, &fd, 4);
-  /* printf("res = %d\n", res); */
+  printf("res = %d\n", res);
   if(res>=0 && fd>=0 && fd<=MAXFD)
     readpos[fd]=-1;
   return res;
@@ -93,7 +93,7 @@ EXTERN_C int read(int fd, void *buf, unsigned int len)
 {
   struct { int fd, pos, len; } cmd;
   int res, tot=0;
-  /* printf("read(%d,%p,%d)\n", fd, buf, len); */
+/*   printf("read(%d,%p,%d)\n", fd, buf, len); */
   if(fd<0 || fd>MAXFD)
     return -1;
   while(len > 1024) {
@@ -108,7 +108,7 @@ EXTERN_C int read(int fd, void *buf, unsigned int len)
   cmd.pos = readpos[fd];
   cmd.len = len;
   res = docmd(2, &cmd, 12);
-  /* printf("res = %d\n", res); */
+/*   printf("res = %d\n", res);  */
   if(res > 0) {
     readpos[fd] += res;
     memcpy(buf, replybuf, res);
@@ -120,7 +120,7 @@ EXTERN_C int pread(int fd, void *buf, unsigned int len, unsigned int offset)
 {
   struct { int fd, pos, len; } cmd;
   int res, tot=0;
-  /* printf("pread(%d,%p,%d,%d)\n", fd, buf, len, offset); */
+/*   printf("pread(%d,%p,%d,%d)\n", fd, buf, len, offset); */
   if(fd<0 || fd>MAXFD)
     return -1;
   while(len > 1024) {
@@ -136,7 +136,7 @@ EXTERN_C int pread(int fd, void *buf, unsigned int len, unsigned int offset)
   cmd.pos = offset;
   cmd.len = len;
   res = docmd(2, &cmd, 12);
-  /* printf("res = %d\n", res); */
+/*   printf("res = %d\n", res);  */
   if(res > 0)
     memcpy(buf, replybuf, res);
   return (res>=0? res+tot : res);
@@ -144,7 +144,7 @@ EXTERN_C int pread(int fd, void *buf, unsigned int len, unsigned int offset)
 
 EXTERN_C long lseek(int fd, long pos, int whence)
 {
-  /* printf("lseek(%d,%d,%d)\n", fd, pos, whence); */
+  printf("lseek(%d,%d,%d)\n", fd, pos, whence); 
   if(fd<0 || fd>MAXFD || readpos[fd]<0)
     return -1;
   switch(whence) {
@@ -160,18 +160,18 @@ EXTERN_C long lseek(int fd, long pos, int whence)
   default:
     return -1;
   }
-  /* printf("res = %d\n", readpos[fd]); */
+  printf("res = %d\n", readpos[fd]); 
   return readpos[fd];
 }
 
 EXTERN_C int file_size(int fd)
 {
   int res;
-  /* printf("file_size(%d)\n", fd); */
+  printf("file_size(%d)\n", fd);
   if(fd<0 || fd>MAXFD || readpos[fd]<0)
     return -1;
   res = docmd(8, &fd, sizeof(fd));
-  /* printf("res = %d\n", res); */
+  printf("res = %d\n", res); 
   return res;
 }
 
@@ -180,9 +180,9 @@ EXTERN_C DIR *opendir(const char *path)
   int res;
   DIR *dirp = malloc(sizeof(DIR));
   if(dirp == NULL) return NULL;
-  /* printf("opendir(%s)\n", path); */
+  printf("opendir(%s)\n", path);
   res = docmd(4, path, strlen(path));
-  /* printf("res = %d\n", res); */
+  printf("res = %d\n", res);
   if(res >= 0) {
     dirp->dd_fd = res;
     dirp->dd_loc = 0;
@@ -194,9 +194,9 @@ EXTERN_C DIR *opendir(const char *path)
 EXTERN_C int closedir(DIR *dirp)
 {
   int res;
-  /* printf("closedir(%p)\n", dirp); */
+  printf("closedir(%p)\n", dirp);
   res = docmd(5, &dirp->dd_fd, sizeof(dirp->dd_fd));
-  /* printf("res = %d\n", res); */
+  printf("res = %d\n", res);
   free(dirp);
   return res;
 }
@@ -207,11 +207,11 @@ EXTERN_C struct dirent *readdir(DIR *dirp)
 {
   struct { int fd, pos; } cmd;
   int res;
-  /*  printf("readdir(%p)\n", dirp); */
+  printf("readdir(%p)\n", dirp);
   cmd.fd = dirp->dd_fd;
   cmd.pos = dirp->dd_loc;
   res = docmd(6, &cmd, 8);
-  /* printf("res = %d\n", res); */
+  printf("res = %d\n", res);
   if(res >= 0) {
     dirp->dd_loc++;
     memcpy(&g_entry, replybuf, replylen);
@@ -224,9 +224,9 @@ EXTERN_C struct dirent *readdir(DIR *dirp)
 EXTERN_C int chdir(const char *path)
 {
   int res;
-  /*  printf("chdir(%s)\n", path); */
+  printf("chdir(%s)\n", path);
   res = docmd(7, path, strlen(path));
-  /* printf("res = %d\n", res); */
+  printf("res = %d\n", res);
   return res;
 }
 
